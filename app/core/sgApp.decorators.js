29803fc6ca7ec14e498c0a56d34209bfc1972ce9
Josh Schneider <josh.schneider@modahealth.com>
2016-11-30 13:29:41 -0800

    updated the numpicker decoration again, much more compact. Fixed an issue with rerendering in groupsize form

--- app/core/sgApp.decorators.js
+++ app/core/sgApp.decorators.js
@@ -19,13 +19,17 @@ const decorators = {
     'ngInject';
     const directive = $delegate[0]; //grab the directive that is being decorated (the first element of the $delegate array)
     directive.scope.ctrl = '='; //this is the vm passed to the numpicker as the "ctrl" attribute
-    const compile = directive.compile; //the compile method of the decorated directive
+    directive.require = '^form'; //make sure the control is nested in a form/ngForm
+    const compile = directive.compile; //ref to the compile method of the decorated directive
     let ctrlName;
     
     directive.compile = function(tElement, tAttrs) { //now we call the compile fn
-      const link = compile.apply(this, arguments); //and create a link variable by calling compile with context and args
+      //and create a link ref variable by calling compile method ref with the directive as context and template el attrs as args
+      const link = compile.apply(this, arguments); 
       //now do decorated compile stuff
       const spans = tElement.find('.input-group-addon');
+      //add a bunch of extra classes and elements for styling
+      //these are all static/structural decorations, so can go in compile instead of link
       angular.element(spans[0])
         .addClass('input-group-addon-left numpicker-button numpicker-decrement')
         .wrapInner('<span class="numpicker-button-text"></span>');
@@ -37,54 +41,35 @@ const decorators = {
         .find('.input-group-addon ~ label')
         .addClass('numpicker-number');
 
-      return function($scope, $elem, $attrs) {
+      return function($scope, $elem, $attrs, formCtrl) {
         const vm = $scope.ctrl;
-        const modelVal = $interpolate($attrs.value)($scope);
         const _val = $scope.value ? $scope.value : 0;
-        let formCtrl;
-        let formCtrlName;
+        // let _formCtrl;
+        // let formCtrlName;
         let inputCtrlName;
         let inputCtrl;
         let $input;
 
         ctrlName = $interpolate($attrs.name)($scope);
-
-        link.apply(this, arguments); //now call link (created within our decorator's compile), with context and args
-        //now do decorated link (postlink) stuff
-        
+        /*now call link ref (created within our decorator's compile), 
+        with context (the directive) and link fn args (scope, elem, attrs, formctrl)*/
+        link.apply(this, arguments); 
+        //now do decorated link (ie, postlink) stuff
         //here is where an input can be inserted, with interpolated ng-model and name attributes
         $elem.find('.numpicker-number')
-          .wrapInner('<input name="' + $attrs.name + '" ng-model="' + $attrs.value + '" class="numpicker-value"></input>');
+          .prepend('<input name="' + $attrs.name + '" ng-model="' + $attrs.value + '" class="numpicker-value">{{$attrs.value}}</input>');
         //this input must now be compiled against scope to register with the parent form (ngModelCtrl)
         $compile($elem.contents())($scope);
-        $input = $elem.find('.numpicker-value');
+        $input = $elem.find('.numpicker-value'); //get a ref to the newly added input
         
-        //get all form control names in the current viewmodel
-        let formCtrlNames = Object.keys(vm).filter((prop) => vm[prop] && vm[prop].constructor && 
-          vm[prop].constructor.name && 
-          vm[prop].constructor.name === 'FormController');
-        if (angular.isArray(formCtrlNames) && formCtrlNames.length > 1) {
-          formCtrlName = formCtrlNames.filter((name) => {
-            return Object.keys(vm[name]).filter((prop) => prop === ctrlName).length > 0;
-          });
-        } else {
-          formCtrlName = angular.isArray(formCtrlNames) ? formCtrlNames[0] : null;
-        }
-        formCtrl = formCtrlName ? vm[formCtrlName] : null;
         inputCtrlName = formCtrl ? Object.keys(formCtrl).filter((prop) => prop === ctrlName) : null;
         inputCtrl = inputCtrlName && formCtrl ? formCtrl[inputCtrlName] : null;
-        $timeout(() => { //kludge to make the view value update on the decorated directive, on load
-          if (inputCtrl && angular.isFunction(inputCtrl.$setViewValue)) {
-            inputCtrl.$setViewValue(_val);
-          }
-          $input.val(_val);
-        });
+
         $scope.$watch(
           () => $scope.value,
           (newVal, oldVal) => {
             if (newVal !== oldVal) {
               const _newVal = newVal ? newVal : 0;
-              $input.val(_newVal);
               if (inputCtrl && angular.isFunction(inputCtrl.$setViewValue)) {
                 inputCtrl.$setViewValue(_newVal);
               }
